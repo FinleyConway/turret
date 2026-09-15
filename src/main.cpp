766ca49fd6dae1f221cc867173fd1a180f0ca7a6
FinleyConway <finleyconway03@gmail.com>
2026-09-15 13:39:49 +0100

    Move towards a class oriented way of setting up a task

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,36 @@
 #include "task.hpp"
 #include "event_queue.hpp"
 
-int main() {
-    event_queue<int> q; 
-    task task([&](std::stop_token token) {
+class test_task {
+public:
+    test_task(event_queue<int>& q) : m_test_q(q) {
+    }
+
+public:
+    void run(std::stop_token token) {
         int i = 0;
 
-        while (q.receive(i, token)) {
+        while (m_test_q.receive(i, token)) {
             std::cout << "Received number: " << i << std::endl;
         }
 
         std::cout << "task ended!\n";
-    });
+    }
+
+private:
+    event_queue<int>& m_test_q;
+};
+
+int main() {
+    event_queue<int> q; 
+    task<test_task> t(q);
 
     q.send(10);
+    q.send(9);
+    q.send(8);
+    q.send(7);
+    q.send(6);
+    q.send(5);
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
--- src/task.hpp
+++ src/task.hpp
@@ -1,15 +1,30 @@
 #pragma once
 
 #include <stop_token>
+#include <concepts>
 #include <utility>
 #include <thread>
 
-template<typename Fn>
+template<typename TWorker>
+concept Worker = requires(TWorker worker, std::stop_token token) {
+    { worker.run(token) }  -> std::same_as<void>;
+};
+
+template<Worker T>
 class task {
 public:
-    explicit task(Fn&& fn) : m_worker([fn = std::forward<Fn>(fn)](std::stop_token token) {
-        fn(token);
-    }) {
+    template<typename... Args>
+    explicit task(Args&&... args)
+        : m_worker([args = std::forward_as_tuple(std::forward<Args>(args)...)](std::stop_token token) {
+            // very messy template wise but it allows me to pass constructor args syntax cleanly
+            std::apply([&](auto&&... args) {
+                T worker(std::forward<decltype(args)>(args)...);
+                    worker.run(token);
+                },
+                args
+            );
+        }) 
+    {
     }
 
     ~task() = default;
@@ -19,9 +34,6 @@ public:
     void stop() {
         m_worker.request_stop();
     }
-
-private:
-    task() = default;
 
 private:
     std::jthread m_worker;
