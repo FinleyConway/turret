9d972a3698254881e910b91245615dbd817169f5
FinleyConway <finleyconway03@gmail.com>
2026-09-16 22:33:36 +0100

    Setup a simple message for motor task

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,54 @@
 #include "task.hpp"
 #include "event_queue.hpp"
 
-class test_task {
+struct stepper_axis {
+    uint16_t tilt_degree = 0;
+    uint16_t pan_degree = 0;
+};
+
+struct motor_control {
+    enum class type {
+        manual,
+        automatic
+    };
+
+    type command;
+    stepper_axis axis;
+};
+
+class motor_task {
 public:
-    test_task(event_queue<int>& q) : m_test_q(q) {
+    explicit motor_task(event_queue<motor_control>& motor_control) : m_motor_control(motor_control) {
     }
 
 public:
     void run(std::stop_token token) {
-        int i = 0;
+        motor_control motor_control;
 
-        while (m_test_q.receive(i, token)) {
-            std::cout << "Received number: " << i << std::endl;
+        while (m_motor_control.receive(motor_control, token)) {
+            stepper_axis axis = motor_control.axis;
+
+            std::cout << "Turning: [x: " << axis.tilt_degree << ", y: " << axis.pan_degree << "]\n";
         }
 
         std::cout << "task ended!\n";
     }
 
 private:
-    event_queue<int>& m_test_q;
+    event_queue<motor_control>& m_motor_control;
 };
 
 int main() {
-    event_queue<int> q; 
-    task<test_task> t(q);
+    event_queue<motor_control> motor;
+    task<motor_task> motor_task(motor);
 
-    q.send(10);
-    q.send(9);
-    q.send(8);
-    q.send(7);
-    q.send(6);
-    q.send(5);
+    motor.send(motor_control { 
+        .command = motor_control::type::manual,
+        .axis = {
+            .tilt_degree = 60,
+            .pan_degree = 360
+        }
+    });
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
 }
