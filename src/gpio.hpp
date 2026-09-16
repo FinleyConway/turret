f454a88ad57ec4e68d4b0de945d38431269a0e9a
FinleyConway <finleyconway03@gmail.com>
2026-09-16 23:18:30 +0100

    Writing a simple gpio abstraction for project

---
+++ src/gpio.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#if HAS_WIRINGPI
+#include <wiringPi.h>
+#endif
+
+enum class gpio_pin {
+    motor_enable = 17, // test values
+    motor_step = 18,
+    motor_direction = 27,
+};
+
+enum class gpio_mode {
+    input,
+    output
+};
+
+class gpio {
+public:
+    static void setup() {
+#if HAS_WIRINGPI
+        wiringPiSetupGpio();
+#endif
+    }
+
+    static void set_mode(gpio_pin pin, gpio_mode mode) {
+#if HAS_WIRINGPI
+        pinMode(
+            static_cast<int>(pin),
+            mode == gpio_mode::output ? OUTPUT : INPUT
+        );
+#endif
+    }
+
+    static void write(gpio_pin pin, bool level) {
+#if HAS_WIRINGPI
+        digitalWrite(
+            static_cast<int>(pin),
+            level ? HIGH : LOW
+        );
+#endif
+    }
+
+    static bool read(gpio_pin pin) {
+#if HAS_WIRINGPI
+        return digitalRead(static_cast<int>(pin)) == HIGH;
+#else
+        return false;
+#endif
+    }
+};
