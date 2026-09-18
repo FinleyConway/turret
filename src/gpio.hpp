4cf53f1a67b9fd89fe1d2307378b73526b6703c6
FinleyConway <finleyconway03@gmail.com>
2026-09-18 11:44:20 +0100

    Add assert checks to gpio wrapper

--- src/gpio.hpp
+++ src/gpio.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
+#include <cassert>
+
 #if HAS_WIRINGPI
 #include <wiringPi.h>
 #endif
 
+// https://github.com/WiringPi/WiringPi/blob/master/documentation/english/functions.md
+
 enum class gpio_pin {
     unconfigured = 0,
-    motor_enable = 17, // test values
-    motor_step = 18,
-    motor_direction = 27,
 };
 
 enum class gpio_mode {
@@ -20,11 +21,16 @@ class gpio {
 public:
     static void setup() {
 #if HAS_WIRINGPI
-        wiringPiSetupGpio();
+        assert(wiringPiSetupPinType(WPI_PIN_PHYS) == 0);
 #endif
+
+        s_setup = true;
     }
 
     static void set_mode(gpio_pin pin, gpio_mode mode) {
+        assert(s_setup && "gpio set was not called");
+        assert(pin != gpio_pin::unconfigured && "Setting a unconfigured pin");
+
 #if HAS_WIRINGPI
         pinMode(
             static_cast<int>(pin),
@@ -34,6 +40,9 @@ public:
     }
 
     static void write(gpio_pin pin, bool level) {
+        assert(s_setup && "gpio set was not called");
+        assert(pin != gpio_pin::unconfigured && "Writing a unconfigured pin");
+
 #if HAS_WIRINGPI
         digitalWrite(
             static_cast<int>(pin),
@@ -43,10 +52,16 @@ public:
     }
 
     static bool read(gpio_pin pin) {
+        assert(s_setup && "gpio set was not called");
+        assert(pin != gpio_pin::unconfigured && "Reading a unconfigured pin");
+
 #if HAS_WIRINGPI
         return digitalRead(static_cast<int>(pin)) == HIGH;
 #else
         return false;
 #endif
     }
+
+private:
+    static inline bool s_setup = false;
 };
